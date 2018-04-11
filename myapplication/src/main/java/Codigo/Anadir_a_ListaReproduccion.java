4a5b905da95ea6b0b4e39b7a6dc6e7a0ed9d7965
aff151 <aff151@inlumine.ual.es>
2018-04-11 11:51:05 +0200

    modificacion añadir a lista

--- myapplication/src/main/java/Codigo/Anadir_a_ListaReproduccion.java
+++ myapplication/src/main/java/Codigo/Anadir_a_ListaReproduccion.java
@@ -1,8 +1,10 @@
+package Codigo;
+
 import java.util.Vector;
 
 public class Anadir_a_ListaReproduccion {
-	private Object _tituloL___Label;
+	/*private Object _tituloL___Label;
 	private Label _seleccionaL;
 	public Visualizacion_Video_Comun_Registrado _unnamed_Visualizacion_Video_Comun_Registrado_;
-	public Vector<Listas_De_Reproduccion> _unnamed_Listas_De_Reproduccion_ = new Vector<Listas_De_Reproduccion>();
+	public Vector<Listas_De_Reproduccion> _unnamed_Listas_De_Reproduccion_ = new Vector<Listas_De_Reproduccion>();*/
 }
