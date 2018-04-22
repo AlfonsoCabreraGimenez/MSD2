1f71dc66cc8d61d4e55809a4ddff96b1a0a2ddde
aff151 <aff151@inlumine.ual.es>
2018-04-22 19:16:06 +0200

    cambios varios

--- myapplication/src/main/java/Codigo/Lista_Videos_Suscriptores.java
+++
@@ -1,8 +0,0 @@
-package Codigo;
-import java.util.Vector;
-
-public class Lista_Videos_Suscriptores {
-	/*private Label _cabeceraL;
-	public Pag_Inicio_R _unnamed_Pag_Inicio_R_;
-	public Vector<Video2> _unnamed_Video2_ = new Vector<Video2>();*/
-}
